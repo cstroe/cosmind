6588cbbbcc26fbe3b04869c9c1211cdbc1f576f9
Cosmin Stroe <cosmin@mailbox.org>
2018-07-13 23:07:57 -0500

    Add Spring security and login form.

--- src/main/java/com/github/cstroe/cosmind/web/IndexController.java
+++ src/main/java/com/github/cstroe/cosmind/web/IndexController.java
@@ -1,12 +1,26 @@
 package com.github.cstroe.cosmind.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
+
 }
